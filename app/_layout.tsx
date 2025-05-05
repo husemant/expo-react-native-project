7a0d021acdafb9670ad65f327c5d0ba1c1a12559
Tanner Huseman <husemant@merrimack.edu>
2025-05-05 14:18:21 -0400

    adding finishing toches

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,15 @@
 import { Stack } from "expo-router";
+
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" />
       <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="+not-found"
-          options={{
-            headerShown: false,
-          }}
-        />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
+      <StatusBar style="light" />
     </>
   );
 }
